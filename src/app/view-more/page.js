232091384bc0f784ce6e686a2db8c774315d65e2
douglas_rondon <douglasgrl27@gmail.com>
2024-06-20 11:51:55 -0400

    sa12

--- src/app/view-more/page.js
+++ src/app/view-more/page.js
@@ -1,31 +1,30 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { fadeIn } from "../../variants";
-import ImagePi from "../../utils/assets/Screenshot-2.png";
-import ImageHyper from "../../utils/assets/image-4-desktop.png";
-import ImagePortafolio from "../../utils/assets/Screenshot_4.png";
+import { fadeIn } from "../variants";
+import ImagePi from "../utils/assets/Screenshot_3.png";
+import ImageHyper from "../utils/assets/hyper-events.png";
+import ImagePortafolio from "../utils/assets/Fronted.png";
 import Image from "next/image";
-import Link from "next/link";
 
-export default function Projects() {
+export default function ViewMore() {
   return (
     <section id="projects" className="section">
-      <div className="container mx-auto px-6 xxl:px-56 lg:mb-20">
+      <div className="container mx-auto px-6 xxl:px-56 lg:mb-20 md:flex md:justify-center">
         <div className="flex flex-col lg:flex-row gap-x-10">
           <motion.div
             variants={fadeIn("right", 0.3)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 flex flex-col gap-y-[3.7rem] mb-5 lg:mb-0 xl:gap-y-[.2rem] xxxl:gap-y-[2.9rem]"
+            className="flex-1 flex flex-col gap-y-[3.7rem] mb-5 lg:mb-0 lg:gap-y-[1.5rem] xl:gap-y-[1rem] xxxl:gap-y-[1.6rem]"
           >
             <div>
               <h2 className="h2 leading-tight text-green">
-                Mis Últimos <br />
-                Trabajos
+                Aca podra visualizar <br />
+                mas trabajos
               </h2>
-              <p className="p max-w-sm xxl:mb-10">
+              <p className="p max-w-sm">
                 Aca les muestro un poco de los proyectos en los que he venido
                 trabajando.
               </p>
@@ -36,7 +35,7 @@ export default function Projects() {
 
               <Image
                 className="group-hover:scale-125 transition-all duration-500 w-full"
-                src={ImageHyper}
+                src={ImagePi}
                 width={400}
                 height={400}
                 alt="HyperEvents"
@@ -48,12 +47,12 @@ export default function Projects() {
               </div>
 
               <div className="absolute -bottom-full left-8 group-hover:bottom-5 transition-all duration-700 z-50">
-                <span className="p text-white">Workitfy</span>
+                <span className="p text-white">Fronted Mentor</span>
               </div>
 
               <div className="absolute -top-full right-6 group-hover:top-[1rem] transition-all duration-700 z-50">
                 <a
-                  href="https://workitfy.maylandlabs.com/"
+                  href="https://douglasgrl.github.io/FrontedMentor-13/"
                   target="_blank"
                   className="p text-white hover:text-white/70"
                 >
@@ -75,7 +74,7 @@ export default function Projects() {
 
               <Image
                 className="group-hover:scale-125 transition-all duration-500 w-full"
-                src={ImagePi}
+                src={ImageHyper}
                 width={400}
                 height={400}
                 alt="HyperEvents"
@@ -87,12 +86,12 @@ export default function Projects() {
               </div>
 
               <div className="absolute -bottom-full left-8 group-hover:bottom-5 transition-all duration-700 z-50">
-                <span className="p text-white">Maylandlabs</span>
+                <span className="p text-white">HyperEvents</span>
               </div>
 
               <div className="absolute -top-full right-6 group-hover:top-[1rem] transition-all duration-700 z-50">
                 <a
-                  href="https://maylandlabs.com/"
+                  href="https://hyperevent-fe.vercel.app/"
                   target="_blank"
                   className="p text-white hover:text-white/70"
                 >
@@ -124,7 +123,13 @@ export default function Projects() {
               </div>
 
               <div className="absolute -top-full right-6 group-hover:top-[1rem] transition-all duration-700 z-50">
-                <Link href="/view-more">Ver mas +</Link>
+                <a
+                  href="https://www.frontendmentor.io/profile/Douglasgrl/solutions"
+                  target="_blank"
+                  className="p text-white hover:text-white/70"
+                >
+                  Ver mas +
+                </a>
               </div>
             </div>
           </motion.div>
